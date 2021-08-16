17865b6a752a4e8a193ee5bdcec5474ec909a410
kisgabor1990 <kisgabor90@gmail.com>
2021-08-16 22:58:54 +0200

    Chat beépítése + hibajavítások

--- src/tictactoe/SzerverGUI.java
+++ src/tictactoe/SzerverGUI.java
@@ -18,35 +18,52 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SzerverGUI {
 
-    private static final int SZELESSEG = 450, MAGASSAG = 540;
-    private static final int KEPERNYO_SZELESSEG = Toolkit.getDefaultToolkit().getScreenSize().width;
-    private static final int KEPERNYO_MAGASSAG = Toolkit.getDefaultToolkit().getScreenSize().height;
+    private final int SZELESSEG = 450, MAGASSAG = 540;
+    private final int KEPERNYO_SZELESSEG = Toolkit.getDefaultToolkit().getScreenSize().width;
+    private final int KEPERNYO_MAGASSAG = Toolkit.getDefaultToolkit().getScreenSize().height;
 
-    public static JFrame szerverAblak;
-    private static JLabel tablaMeretLabel, portLabel;
-    private static JFormattedTextField tablaMeretText, portText;
-    private static JCheckBox csatlakozasInditasUtan;
-    private static JButton inditasGomb;
-    private static JTextArea log;
-    private static JScrollPane logPane;
+    public JFrame szerverAblak;
+    private JLabel tablaMeretLabel, portLabel;
+    private JFormattedTextField tablaMeretText, portText;
+    private JCheckBox csatlakozasInditasUtan;
+    private JButton inditasGomb;
+    private JTextArea log;
+    private JScrollPane logPane;
 
-    private static MaskFormatter tablaMeretFormatter, portFormatter;
-    private static String IPcim;
-    private static boolean inditva = false;
-    private static int port;
+    private MaskFormatter tablaMeretFormatter, portFormatter;
+    private String IPcim;
+    private boolean inditva = false;
+    private int port, jelenlegiJatekosok = 0;
 
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("[HH:mm:ss] ");
-    private static ExecutorService es = Executors.newFixedThreadPool(2);
-    private static ServerSocket serverSocket;
-    private static List <Jatekos> jatekosok = new ArrayList <>();
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("[HH:mm:ss] ");
+    private ExecutorService es = Executors.newCachedThreadPool();
+    private ServerSocket serverSocket;
+    private List <Jatekos> jatekosok = new ArrayList <>();
+    private DefaultCaret caret;
 
-    private static DefaultCaret caret;
+    public void szerverMain() {
+        IPcim = getIP();
+        try {
+            tablaMeretFormatter = new MaskFormatter("##");
+            portFormatter = new MaskFormatter("####");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        szerverAblak();
+        komponensek();
+        szerverAblak.setVisible(true);
+
+        caret = (DefaultCaret) log.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+    }
 
 
-    public static void szerverAblak() {
+    public void szerverAblak() {
         szerverAblak = new JFrame("Szerver indítása");
         szerverAblak.setSize(SZELESSEG, MAGASSAG);
         szerverAblak.setLocation((KEPERNYO_SZELESSEG - SZELESSEG) / 2, (KEPERNYO_MAGASSAG - MAGASSAG) / 2);
@@ -55,37 +72,26 @@ public class SzerverGUI {
         szerverAblak.setLayout(null);
     }
 
-    public static void komponensek() {
+    public void komponensek() {
         tablaMeretLabel = new JLabel("Tábla mérete:", SwingConstants.RIGHT);
         tablaMeretLabel.setSize(300, 40);
         tablaMeretLabel.setLocation(20, 20);
         tablaMeretLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
         szerverAblak.add(tablaMeretLabel);
 
-        try {
-            tablaMeretFormatter = new MaskFormatter("##");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
         tablaMeretText = new JFormattedTextField(tablaMeretFormatter);
-        tablaMeretText.setText("99");
+        tablaMeretText.setText("35");
         tablaMeretText.setSize(80, 40);
         tablaMeretText.setLocation(330, 20);
         tablaMeretText.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
         szerverAblak.add(tablaMeretText);
 
-        IPcim = getIP();
         portLabel = new JLabel("IP: " + IPcim + " Port:", SwingConstants.RIGHT);
         portLabel.setSize(300, 40);
         portLabel.setLocation(20, 80);
         portLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
         szerverAblak.add(portLabel);
 
-        try {
-            portFormatter = new MaskFormatter("####");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
         portText = new JFormattedTextField(portFormatter);
         portText.setText("9000");
         portText.setSize(80, 40);
@@ -108,8 +114,8 @@ public class SzerverGUI {
 
         log = new JTextArea();
         log.setEditable(false);
-        caret = (DefaultCaret) log.getCaret();
-        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        log.setLineWrap(true);
+        log.setWrapStyleWord(true);
 
         logPane = new JScrollPane(log);
         logPane.setSize(400, 200);
@@ -117,84 +123,101 @@ public class SzerverGUI {
         szerverAblak.add(logPane);
     }
 
-    private static void szerverInditas() {
+    private void szerverInditas() {
         port = Integer.parseInt(portText.getText());
 
         if (!inditva) {
             tablaMeretText.setEnabled(false);
             portText.setEnabled(false);
             csatlakozasInditasUtan.setEnabled(false);
-            inditva = true;
-
+            inditasGomb.setEnabled(false);
 
             log.append(dateFormat.format(new Date()) + "Szerver indítása...\n");
-            Thread openPortThread = new Thread(() -> {
-                inditasGomb.setEnabled(false);
-                try {
-                    serverSocket = new ServerSocket(port);
-                } catch (IOException e) {
-                    log.append(dateFormat.format(new Date()) + "Nem sikerült a szerver indítása!\n");
-                    log.append(dateFormat.format(new Date()) + e + "\n");
-                }
+            try {
+                serverSocket = new ServerSocket(port);
+                log.append(dateFormat.format(new Date()) + "Szerver elindítva.\n");
+            } catch (IOException e) {
+                log.append(dateFormat.format(new Date()) + "Nem sikerült a szerver indítása!\n");
+                log.append(dateFormat.format(new Date()) + e + "\n");
+            }
+            log.append(dateFormat.format(new Date()) + "Port nyitása...\n");
+
+            es.submit(() -> {
                 UPnP.openPortTCP(port);
+                log.append(dateFormat.format(new Date()) + "Port nyitva: " + port + "\n");
                 inditasGomb.setText("Leállítás");
                 inditasGomb.setEnabled(true);
                 log.append(dateFormat.format(new Date()) + "A szerver várakozik a következő porton: " + port + "\n");
-                while (inditva) {
+                inditva = true;
+
+                while (inditva && jelenlegiJatekosok != 2) {
                     Jatekos jatekos = new Jatekos();
                     try {
                         jatekos.setSocket(serverSocket.accept());
                         jatekosok.add(jatekos);
+                        jelenlegiJatekosok = jatekosok.size();
 
                         es.submit(() -> {
                             try {
                                 BufferedReader bemenet = new BufferedReader(new InputStreamReader(jatekos.getSocket().getInputStream()));
                                 PrintWriter kimenet = new PrintWriter(jatekos.getSocket().getOutputStream(), true);
-
                                 if (jatekosok.size() == 1) {
                                     jatekos.setXO("X");
                                 } else {
                                     jatekos.setXO("O");
                                 }
-                                kozvetit(jatekos.getXO() + " játékos becsatlakozot.");
-                                log.append(dateFormat.format(new Date()) + jatekos.getXO() + " játékos becsatlakozott.");
+
+                                jatekos.setNev(bemenet.readLine());
+                                kimenet.println(tablaMeretText.getText());
+
+                                kimenet.println(dateFormat.format(new Date()) + "Üdvözöllek " + jatekos.getNev() + "!");
+                                log.append(dateFormat.format(new Date()) + jatekos.getXO() + " játékos becsatlakozott (" + jatekos.getNev() + ").\n");
+                                kozvetit(jatekos.getXO() + " játékos becsatlakozott (" + jatekos.getNev() + ").");
+                                String uzenet;
+                                while ((uzenet = bemenet.readLine()) != null) {
+                                    log.append(dateFormat.format(new Date()) + "[" + jatekos.getNev() + "] " + uzenet + "\n");
+                                    kozvetit("[" + jatekos.getNev() + "] " + uzenet);
+                                }
                             } catch (IOException e) {
                                 jatekosok.remove(jatekos);
-                                log.append(dateFormat.format(new Date()) + e + "\n");
+                                log.append(dateFormat.format(new Date()) + jatekos.getXO() + " játékos kilépett.\n");
                             }
                         });
                     } catch (IOException e) {
                         jatekosok.remove(jatekos);
                     }
                 }
+
             });
-            openPortThread.start();
-
-
         } else {
+            inditva = false;
+            jatekosok.clear();
+            jelenlegiJatekosok = 0;
             tablaMeretText.setEnabled(true);
             portText.setEnabled(true);
             csatlakozasInditasUtan.setEnabled(true);
-            inditva = false;
 
             log.append(dateFormat.format(new Date()) + "Szerver leállítása...\n");
-            new Thread(() -> {
-                inditasGomb.setEnabled(false);
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    log.append(dateFormat.format(new Date()) + "Nem sikerült a szerver leállítása!\n");
-                    log.append(dateFormat.format(new Date()) + e + "\n");
-                }
+            inditasGomb.setEnabled(false);
+            try {
+                serverSocket.close();
+                log.append(dateFormat.format(new Date()) + "Szerver leállítva.\n");
+            } catch (IOException e) {
+                log.append(dateFormat.format(new Date()) + "Nem sikerült a szerver leállítása!\n");
+                log.append(dateFormat.format(new Date()) + e + "\n");
+            }
+            log.append(dateFormat.format(new Date()) + "Port lezárása...\n");
+            es.submit(() -> {
                 UPnP.closePortTCP(port);
+                log.append(dateFormat.format(new Date()) + "Port lezárva: " + port + "\n");
                 inditasGomb.setText("Indítás");
                 inditasGomb.setEnabled(true);
-                log.append(dateFormat.format(new Date()) + "A szerver leállt.\n");
-            }, "closePortThread").start();
+            });
         }
+
     }
 
-    public static String getIP() {
+    public String getIP() {
         String myip = "";
         try {
             URL checkip = new URL("http://checkip.amazonaws.com");
@@ -206,15 +229,19 @@ public class SzerverGUI {
         return myip;
     }
 
-    private static void kozvetit(String uzenet) {
+    private void kozvetit(String uzenet) {
+        PrintWriter sKimenet;
         for (Jatekos j : jatekosok) {
-            PrintWriter sKimenet = null;
             try {
                 sKimenet = new PrintWriter(j.getSocket().getOutputStream(), true);
                 sKimenet.println(dateFormat.format(new Date()) + uzenet);
             } catch (IOException e) {
-                log.append(dateFormat.format(new Date()) + e + "\n");
+                e.printStackTrace();
             }
         }
+    }
+
+    private boolean csatlakozvaVan(Socket socket) {
+        return false;
     }
 }
